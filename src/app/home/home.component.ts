3c16cd474998dca7cfd5cc9b780005f2741dc189
ashefor <thatsureboy@gmail.com>
2019-04-25 17:56:28 +0100

    fixed side menu for mobile view with event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -10,45 +10,49 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class HomeComponent implements OnInit {
   showmenu = false;
   showmenu2 = true;
-  hidepost= false;
-  constructor(private http: HttpClient, private fb: FormBuilder) { }
+  hidepost = false;
+  
+
+  constructor(private http: HttpClient, private fb: FormBuilder, private element: ElementRef) {}
 
   ngOnInit() {
-    // this.resizeWindow()
+    this.element.nativeElement.addEventListener('click', this.onMouseEnter);
   }
 
+  ngOnDestroy() {
+    this.element.nativeElement.removeEventListener('click', this.onMouseEnter);
+  }
+
+  onMouseEnter() {
+    let box1 = document.getElementById('box1')
+    let box3 = document.querySelectorAll('.side-containers-content')
+    let box = document.getElementById('lightmenu')
+    let box2 = document.getElementById('side-container-right')
+    let children = box3[1]
+    //  alert("Don't touch my bacon!");
+     if(event.target == box){
+      // box.style.display = 'none';
+      console.log('test')
+      box2.style.display = 'block'
+     }else if(event.target == box2){
+      box2.style.display = 'none'
+     }
+     
+  }
+  
   // callApi(){
   //   this.http.get('https://reqres.in/api/users?page=2').subscribe(data => {
   //     console.log(data)
   //   })
   // }
-  
+
   subscriptionForm = this.fb.group({
     email: ['', Validators.compose([Validators.required])],
   })
 
-  submitForm(){
-    window.alert('me')
+  submitForm() {
+    window.alert('submitted')
   }
+  
 
-  default(event){
-    event.preventDefault()
-  }
-  resizeWindow(){
-    if(window.outerWidth <= 479){
-        this.showmenu = true;
-    }else{
-      this.showmenu2 = false;
-    }
-  }
-
-  // closedrop(event){
-  //   let box = document.querySelector('.dropdown-content')
-  //   document.addEventListener("click", (event)=>{
-  //     if(event.target.className === '.dropdown.content'){
-  //       box.classList.add(".hide")
-  //       console.log(event.target.className)
-  //     }
-  //   })
-  // }
 }
